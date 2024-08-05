7d7ffc9609af6fa3d128b3a0cbe52e018601558f
Tessmore <fabientesselaar@gmail.com>
2024-08-05 19:14:28 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>PLACEHOLDER_TITLE</h1>
+        <p>Get started</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
